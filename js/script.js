7a073e6e828ddb19bb6676ebfedfb9d9b585a81a
Rustam Botirov <rustambotirov88@gmail.com>
2019-05-30 19:47:17 +0300

    first section coded; fix header

--- js/script.js
+++ js/script.js
@@ -1,36 +1,21 @@
 $(document).ready(function() {
-
-    $('.carousel').slick({
-        asNavFor: '.carousel-bg',
-        dots: true
-    });
-    $('.carousel-bg').slick({
-        arrows: false,
-        asNavFor: '.carousel',
-        swipe: false,
-        fade: true
-    });
-
-    $('.posts-big-carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1
-    });
-    $('.posts-small-carousel').slick({
+    $('.banner-carousel').slick({
+        fade: true,
         autoplay: true,
         arrows: false,
-        dots: true,
-    });
-    $('.burger').click(function() {
-        $('.nav-container').toggleClass('active');
-        $('.burger').toggleClass('active');
-    });
+        asNavFor: '.dots-wrap',
 
-    // $('.play-video').fancybox({
-    //     'width'				: '75%',
-    //     'height'			: '75%',
-    //     'autoScale'			: false,
-    //     'transitionIn'		: 'none',
-    //     'transitionOut'		: 'none',
-    //     'type'				: 'iframe'
+
+    });
+    $('.dots-wrap').slick({
+        slidesToShow: 3,
+        autoplay: true,
+        asNavFor: '.banner-carousel',
+        focusOnSelect: true
+
+    });
+    // $('.burger').click(function() {
+    //     $('.nav-container').toggleClass('active');
+    //     $('.burger').toggleClass('active');
     // });
 });
