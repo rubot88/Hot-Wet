4a3b50356a71719480de45e1c08de3990192a259
Rustam Botirov <rustambotirov88@gmail.com>
2019-06-05 21:36:23 +0300

    Footer completed; script.js modified

--- js/script.js
+++ js/script.js
@@ -24,16 +24,34 @@ $(document).ready(function() {
         $('.moreless').toggleClass('opened');
         if ($('.moreless').hasClass('opened')) {
             $('.section-text').toggleClass('expanded');
+            $('.section-text').css('height', 'auto');
+            var height_expand = $('.section-text').height();
+            $('.section-text').css('height', '80px');
+            $('.section-text').animate({ height: height_expand }, 300);
             $('.moreless').text('Свернуть');
 
         } else {
-            $('.section-text').removeClass('expanded');
+            $('.section-text').removeClass('expanded').animate({ height: '80px' }, 300);;
             $('.moreless').text('Развернуть');
 
         }
 
         return false;
     });
+
+
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > ($('header').offset().top + $('header').outerHeight() - $('.nav-section').height())) {
+            var height_nav = $('.nav-section').outerHeight();
+            $('header').css({ 'height': $('header').height() });
+            $('.nav-section').addClass('top-fixed');
+        } else {
+            $('header').removeAttr('style');
+            $('.nav-section').removeClass('top-fixed');
+        }
+    });
+
 
     // $('.burger').click(function() {
     //     $('.nav-container').toggleClass('active');
